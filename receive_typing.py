b563f71bd0735d9f54f2ed2b8bc75ed8bff22845
HARB HAMZA <HARB@Unicel.com>
2025-12-30 13:37:13 -0500

    added all cases for receiving, next work on user input of sublocation and sku's to receive it on

--- receive_typing.py
+++ receive_typing.py
@@ -18,14 +18,22 @@ with open("/Users/hamza/Desktop/Programma Uscita Pulita/receive_data.txt", "r") 
     imeis = [line.strip() for line in f if line.strip()]
 
 print(f"Processing {len(imeis)} barcodes...")
-
+# first item handled separately
 type_text(imeis[0])
 time.sleep(0.1)
 press_enter()
 time.sleep(0.1)
+# receiving items in amazon sku, good conditions start with good and its variations
+# acceptable starts with acceptable and its variations
+startCases = ["Good", "good","GOOD","goood", "Acceptable", "acceptable", "ACCEPTABLE","Accep","accep"]
+# receiving in Excellent condition amazon SKU, they always contain '/' inside the SKU
+containCases = ["/"]
+# in the case we receive in generic SKU's we check for two spaces in the entire string
+# count of spaces inside string must be 2 to trigger this case
+# imei.count(' ') == 2 used for that case
 for i, imei in enumerate(imeis[1::], 1):
     print(f"[{i}/{len(imeis)}] {imei}")
-    if imei.startswith("G") or imei.startswith("g") :
+    if imei.startswith(startCases) or imei.contains(containCases) or imei.count(' ') == 2:
         time.sleep(0.1)
         print("iphone found")
         time.sleep(0.1)
